f8ccac678a3e24a4b4b7280efa76a9bff0edcd42
Samuel <samnmbuguah@gmail.com>
2023-03-15 14:42:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let charges = 20
+  if(feet < 401){
+    return 'This one is on me!';
+  }
+  else if (feet > 2000 && feet <2501 ){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feet >= 2501){
+    return 'No can do.'
+  }
+  else{
+    return `${charges}`
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let canGo = city ==='NYC'? "Ok, sounds good." : "No go.";
+  return canGo
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charm){
+  switch(charm) {
+    case "generous" : return "Thank you so much."
+    break;
+    case "not as generous" : return "Thank you."
+    break;
+    default : return "Bye."
+    break;
+  }
+  
 }
